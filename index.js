606487519f17cbc60e01dd819b1ebe9cdd2761b2
Yeshwant-22 <yeshwant.rajaraman@cdw.com>
2024-09-17 17:09:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 async function initialFetchCloudWatchData() {
-    let baseURL = "https://vrx0x2tili.execute-api.us-east-1.amazonaws.com/testing/yamlcloudwatchtest";
+    let baseURL = "https://2pba08y0dk.execute-api.us-east-1.amazonaws.com/testing/yamlcloudwatchtest";
     try {
        let response = await fetch(baseURL);
         if (!response.ok) {
